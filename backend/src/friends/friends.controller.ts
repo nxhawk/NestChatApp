504ba93bb9e3d38b9add127f9f49a663ba4ba35e
21120447 <haonhat2729@gmail.com>
2024-04-06 16:27:26 +0700

    get list friends, delete friend relation

--- backend/src/friends/friends.controller.ts
+++ backend/src/friends/friends.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { SkipThrottle } from '@nestjs/throttler';
+import { Routes, Services } from 'src/utils/constants';
+import { IFriendsService } from './friends';
+import { AuthUser } from 'src/utils/decorators';
+import { User } from 'src/utils/typeorm';
 
-@Controller('friends')
-export class FriendsController {}
+@SkipThrottle()
+@Controller(Routes.FRIENDS)
+export class FriendsController {
+  constructor(
+    @Inject(Services.FRIENDS_SERVICE)
+    private readonly friendsService: IFriendsService,
+  ) {}
+
+  @Get()
+  getFriends(@AuthUser() user: User) {
+    return this.friendsService.getFriends(user.id);
+  }
+
+  @Delete(':id/delete')
+  async deleteFriend(
+    @AuthUser() { id: userId }: User,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    const friend = await this.friendsService.deleteFriend({ id, userId });
+    // socket here
+    return friend;
+  }
+}
--- backend/src/friends/friends.service.ts
+++ backend/src/friends/friends.service.ts
@@ -4,6 +4,8 @@ import { Friend } from 'src/utils/typeorm';
 import { DeleteFriendRequestParams } from 'src/utils/types';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { FriendNotFoundException } from './exceptions/FriendNotFound';
+import { DeleteFriendException } from './exceptions/DeleteFriend';
 
 @Injectable()
 export class FriendsService implements IFriendsService {
@@ -13,15 +15,47 @@ export class FriendsService implements IFriendsService {
   ) {}
 
   getFriends(id: number): Promise<Friend[]> {
-    throw new Error('Method not implemented.');
+    return this.friendsRepository.find({
+      where: [
+        {
+          sender: { id },
+        },
+        {
+          receiver: { id },
+        },
+      ],
+      relations: [
+        'sender',
+        'receiver',
+        'sender.profile',
+        'receiver.profile',
+        'receiver.presence',
+        'sender.presence',
+      ],
+    });
   }
 
   findFriendById(id: number): Promise<Friend> {
-    throw new Error('Method not implemented.');
+    return this.friendsRepository.findOne({
+      where: { id },
+      relations: [
+        'sender',
+        'receiver',
+        'sender.profile',
+        'sender.presence',
+        'receiver.profile',
+        'receiver.presence',
+      ],
+    });
   }
 
-  deleteFriend(params: DeleteFriendRequestParams) {
-    throw new Error('Method not implemented.');
+  async deleteFriend({ id, userId }: DeleteFriendRequestParams) {
+    const friend = await this.findFriendById(id);
+    if (!friend) throw new FriendNotFoundException();
+    if (friend.receiver.id !== userId && friend.sender.id !== userId)
+      throw new DeleteFriendException();
+    await this.friendsRepository.delete(id);
+    return friend;
   }
 
   isFriends(userOneId: number, userTwoId: number): Promise<Friend> {
